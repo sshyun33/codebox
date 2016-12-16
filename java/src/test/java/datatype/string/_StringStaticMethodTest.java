148cf364e0ff144781029ce4bd67a1d1a7cbf5df
Seo Seugnhyun <sshyun33@gmail.com>
2016-12-16 15:39:59 +0900

    add String static method(startswith(),endswith())

--- java/src/test/java/datatype/string/_StringStaticMethodTest.java
+++ java/src/test/java/datatype/string/_StringStaticMethodTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class _StringStaticMethodTest {
 
@@ -30,11 +31,19 @@ public class _StringStaticMethodTest {
     }
 
     @Test
-    public void test2() throws Exception {
+    public void getCharacterOrCodePointFromIndex() throws Exception {
         text = "This is sample text.";
-
+        // get a character from the index
         assertThat(text.charAt(1), is('h'));
-        // get unicode code point from index
+        // get a unicode codepoint from the index
         assertThat(text.codePointAt(1), is(104));
+    }
+
+    @Test
+    public void testStringStartsOrEndsWith() throws Exception {
+        text = "This is sample text.";
+        assertTrue(text.startsWith("This"));
+        assertTrue(text.startsWith("is", 5));
+        assertTrue(text.endsWith("text."));
     }
 }
